7060fbdb62451b949efd785138794a20070ab778
fdaffa12 <faizdaffa17@gmail.com>
2024-12-27 22:56:22 +0700

    Enhance RUN DISINI.bat with additional user guidance messages for troubleshooting

--- RUN DISINI.bat
+++ RUN DISINI.bat
@@ -1,4 +1,6 @@
 @echo off
 echo Starting Helmet Detection Program...
+echo JIKA SCRIPT TIDAK BERFUNGSI TOLONG BACA FILE README.MD
+echo SEMUA JAWABAN ADA DISANA.. :)
 python helmet_detection_video.py
 pause
